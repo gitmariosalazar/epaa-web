2c94fe1ae37e90fdfe116a7176c3181cdaddbdc1
Mario Salazar <152939674+gitmariosalazar@users.noreply.github.com>
2026-08-03 15:24:21 -0500

    feat: upload new changes

--- src/modules/incidents/presentation/components/AdditionalInfoAccordion.tsx
+++ src/modules/incidents/presentation/components/AdditionalInfoAccordion.tsx
@@ -14,6 +14,7 @@ import {
   BookOpen
 } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import '@/modules/readings/presentation/styles/create-reading.css';
 import { ConnectionStateChip } from '@/shared/presentation/components/chip/ConnectionStateChip';
 import { GrStatusUnknown } from 'react-icons/gr';
 import type { ReadingInfo } from '@/modules/readings/domain/models/ReadingInfoResponse';
